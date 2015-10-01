4794bfe5ba7d2ad93dc8a6df4b37223ce3776ed3
Hidden666 <shadow@amp.pw.edu.pl>
2015-10-01 17:01:51 +0200

    Commit 1

---
+++ MoviesApplication/Controllers/API/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Data;
+using Data.Interfaces;
+using Infrastructure;
+using Membership.Interfaces;
+
+namespace MoviesApplication.Controllers.API
+{
+    [Authorize(Roles="Admin")]
+    [RoutePrefix("api/Account")]
+    public class AuthenticationController : APIControllerBase
+    {
+        private readonly IMembershipService membershipService;
+
+        public AuthenticationController(IMembershipService membershipService, IEntityBaseRepo<Error> errorRepository, IUnitOfWork unitOfWork)
+            : base(unitOfWork, errorRepository)
+        {
+            this.membershipService = membershipService;
+        }
+
+
+    }
+}
