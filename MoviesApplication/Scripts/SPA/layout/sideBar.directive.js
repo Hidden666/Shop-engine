ee31a717b140727ed6adbbf3fda5dfc83efd5ddd
Hidden666 <shadow@amp.pw.edu.pl>
2015-11-27 18:06:16 +0100

    commit 12

--- MoviesApplication/Scripts/SPA/layout/sideBar.directive.js
+++ MoviesApplication/Scripts/SPA/layout/sideBar.directive.js
@@ -3,12 +3,34 @@
 
     app.directive('sideBar', sideBar);
 
+    app.controller('sideBarController', sideBarController);
+
+    sideBarController.$inject = ["$scope"];
+
+    function sideBarController($scope) {
+
+        $scope.status = {
+            isopen: false
+        };
+
+        $scope.toggled = function (open) {
+            $log.log('Dropdown is now: ', open);
+        };
+
+        $scope.toggleDropdown = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.status.isopen = !$scope.status.isopen;
+        };
+    }
+
     function sideBar() {
         return {
             restrict: 'E',
             replace: true,
-            templateUrl: '/scripts/spa/layout/sideBar.html'
-        };
+            templateUrl: '/scripts/spa/layout/sideBar.html',
+            controller: sideBarController
+    };
     };
 
 
